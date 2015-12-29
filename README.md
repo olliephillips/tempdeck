627a1887dbc5532f63ca877418158a36cdad0cd1
Ollie Phillips <oliver@eantics.co.uk>
2015-12-29 21:24:43 +0000

    First go

---
+++ README.md
@@ -0,0 +1,77 @@
+# tempdeck
+
+Monitor temperature over MQTT (currently you'll need to build your own client or use a tool), or just wait for alerts if/when temperature goes beyond tolerance.
+
+## About
+
+Tempdeck is a temperature monitoring application for Espruino on ESP 8266. Records temperature over MQTT. Sends alert messages - when temperature goes "out of tolerance" - via IFTTT Maker channel. 
+
+There's much more that can be done here, for example local and remote control of temperature. Much of this I already have working as a prototype, but some of it is dependent on how the "Espruino for ESP 8266" port is finalised. The port itself is currently in development.
+
+## Setup
+
+Assumes running Espruino on ESP 8266. It will run on any ESP 8266 board, only one GPIO is required.
+Download and load the module locally, or use it directly from this repo. There is a Closure minified version included. 
+
+Requires a DS18B20 temperature sensor.
+
+This module could easilly be modified to run on Pico and original Espruino board.
+
+You will need an IFTTT account (free), and to activate and configure the "Maker" channel to trigger the "that" of your choice. I find the SMS alerts to be convenient. For example, for an SMS message, you can use this script. 
+
+```
+Check your fermentation!! Current temp: {{Value1}} Target temp: {{Value2}} Time: {{OccurredAt}}
+```
+Both "Value1" (current temp) and "Value2" (target temp) parameters are provided by this module, just include them in your action. "OccurredAt" is an IFTTT timestamp, no need to worry about it.
+
+## Example   
+
+```
+var tempdeck = require("tempdeck").tempdeck;
+
+// Below are the only required configuration options
+
+// Configure
+tempdeck.config.wifi = {
+    ssid:	"MyWifiNetwork",
+    pwd:	"myPassword"
+};
+
+tempdeck.config.ifttt = {
+  key:		"nviy7SzmyiftttkeyufaEDSzsmy-anC98_wfbh",
+  event:	"temp_tolerance"
+};
+
+tempdeck.init();
+
+```
+
+## Config Options
+
+The ```tempdeck.config``` object has the following defaults. 
+
+```
+config: {
+    wifi: {
+      ssid:       "",
+      password:   ""
+    },
+    pinIn:        "D0",
+    target:       20,
+    tolerance:    2,
+    interval:     5,
+    serial:  getSerial(), // This will return ESP8266 MAC address
+    mqtt: {
+      server:     "test.mosquitto.org",
+      topic :     "tempdeck/esp8266/"
+    },
+    ifttt: {
+      key:        "",
+      event:      ""
+    }
+  },
+```
+
+## To do
+
+- A client for subscribing to the MQTT topic for the purpose of monitoring the fermentation
---
+++ tempdeck.js
@@ -0,0 +1,106 @@
+// Tempdeck. Remote temperature monitoring and alerts
+// Written for Espruino running on ESP8266
+// Copyright 2016, MIT License
+
+var tempdeck = {
+  // Configuration defaults
+  config: {
+    wifi: {
+      ssid:       "",
+      password:   ""
+    },
+    pinIn:        "D0",
+    target:       20,
+    tolerance:    2,
+    interval:     5,
+    serial:  getSerial(),
+    mqtt: {
+      server:     "test.mosquitto.org",
+      topic :     "tempdeck/esp8266/"
+    },
+    ifttt: {
+      key:        "",
+      event:      ""
+    }
+  },
+  // Start up
+  init: function(){ 
+    // Setup OneWire
+    pinMode(tempdeck.config.pinIn, 'input');
+    tempdeck.ow = new OneWire(tempdeck.config.pinIn);
+    tempdeck.probe = require("DS18B20").connect(tempdeck.ow);
+    
+    // Setup Wifi & connect
+    tempdeck.wifi = require("Wifi");
+    //tempdeck.wifi.stopAP();
+    tempdeck.wifi.connect(tempdeck.config.wifi.ssid,{password: tempdeck.config.wifi.pwd},
+      function(params) {
+        console.log("Connected..");
+        // Setup MQTT
+        tempdeck.mqttSetup();
+        tempdeck.monitor();
+      }
+    );
+  }, 
+  // Manage MQTT connection
+  mqttSetup: function() {
+    tempdeck.mqtt = null;
+    tempdeck.mqtt = require("https://github.com/olliephillips/tinyMQTT/blob/master/tinyMQTT.min.js")
+      .create(tempdeck.config.mqtt.server);
+    tempdeck.mqtt.connect();
+    tempdeck.mqtt.on("connected", function(){
+      tempdeck.send = true;
+      console.log("MQTT Server: '"+ tempdeck.config.mqtt.server +"', Topic: '" + 
+                  tempdeck.config.mqtt.topic + tempdeck.config.serial + "'");
+      tempdeck.mqtt.on("disconnected", function(){
+        tempdeck.send = false;
+        console.log("Disconnected");
+        tempdeck.mqttSetup();
+      });
+    });
+  },
+  // Routine to record and log temperature
+  monitor: function() {
+    tempdeck.monitorCount = 0;
+    tempdeck.onAlert = false;
+    setInterval(function(){
+      tempdeck.curTemp = tempdeck.probe.getTemp().toFixed(2);
+      if(tempdeck.monitorCount > 0) { // Ignore first read
+        console.log("Current temp is " + tempdeck.curTemp);
+        // Record over MQTT
+        if(tempdeck.send === true) {
+          tempdeck.message = {
+            targetTemp: tempdeck.config.target,
+            currentTemp: tempdeck.curTemp
+          };
+          tempdeck.mqtt.publish("tempdeck/esp8266/" + tempdeck.config.serial, JSON.stringify(tempdeck.message));
+        }  
+        // Within tolerance?
+        tempdeck.variance = tempdeck.config.target - tempdeck.curTemp;
+        if(tempdeck.variance < 0 ) {
+          tempdeck.variance = tempdeck.variance * -1;
+        }
+        if(tempdeck.variance > tempdeck.config.tolerance) {
+            // Check if alert already been sent
+            if(tempdeck.onAlert == false) {
+              // Fire alert
+              tempdeck.alert();
+            }
+        }
+      }
+      tempdeck.monitorCount++;
+    }, tempdeck.config.interval * 1000);  
+  }, 
+  // IFTTT alert notification
+  alert: function(){
+    tempdeck.alertTarget =  "http://maker.ifttt.com/trigger/" + tempdeck.config.ifttt.event + "/with/key/"+ tempdeck.config.ifttt.key;
+    tempdeck.alertData = "?value1=" + tempdeck.curTemp + "&amp;value2=" + tempdeck.config.target;
+    require("http").get(tempdeck.alertTarget + tempdeck.alertData, function(res) {
+    res.on('close', function() {
+      tempdeck.onAlert = true;
+      console.log("Alert sent");
+    });
+  });
+  } 
+};
+exports.tempdeck = tempdeck;
